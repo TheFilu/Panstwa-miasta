2fbdb7d924ce7d67840917fdc7a74d01c1a1e11c
TheFilu <dawid.fil97@gmail.com>
2026-02-08 14:50:19 +0000

    Update: deployment from development

--- server/static.ts
+++ server/static.ts
@@ -10,14 +10,7 @@ export function serveStatic(app: Express) {
     );
   }
 
-  app.use(express.static(distPath, { 
-    // Ensure proper MIME types
-    setHeaders: (res, filepath) => {
-      if (filepath.endsWith('.js')) res.setHeader('Content-Type', 'application/javascript');
-      if (filepath.endsWith('.css')) res.setHeader('Content-Type', 'text/css');
-      if (filepath.endsWith('.json')) res.setHeader('Content-Type', 'application/json');
-    }
-  }));
+  app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
